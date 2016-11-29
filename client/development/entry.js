6d5bbc4e639e096d725db4e6711483307c13b127
Luis Herranz <luisherranz@gmail.com>
2016-11-29 17:38:27 +0100

    Add development package.

---
+++ client/development/entry.js
@@ -0,0 +1,24 @@
+import 'worona-polyfills';
+import { packageDevelopment } from 'worona-deps';
+import pkgJson from './test-development-package-worona/package.json';
+import * as pkg from './test-development-package-worona/src/app';
+
+packageDevelopment({
+  name: pkgJson.name,
+  namespace: pkgJson.worona.namespace,
+  woronaInfo: {
+    name: pkgJson.name,
+    id: pkgJson.name,
+    namespace: pkgJson.worona.namespace,
+    niceName: pkgJson.worona.niceName,
+    type: pkgJson.worona.type,
+    menu: {
+      category: 'Development',
+      order: 10,
+    },
+    services: pkgJson.worona.services,
+  },
+  ...pkg,
+});
+
+console.log('test-development-package-worona loaded!');
