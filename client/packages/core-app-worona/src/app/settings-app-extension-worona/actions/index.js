ee0059e0cdfb6da3a860dec27a802a56ae0095b8
Luis Herranz <luisherranz@gmail.com>
2017-01-10 13:34:12 +0100

    Add settingsUpdated action.

--- client/packages/core-app-worona/src/app/settings-app-extension-worona/actions/index.js
+++ client/packages/core-app-worona/src/app/settings-app-extension-worona/actions/index.js
@@ -6,3 +6,6 @@ export const appSettingsSucceed = ({ settings, pkgs }) =>
   ({ type: types.APP_SETTINGS_SUCCEED, settings, pkgs });
 export const appSettingsFailed = ({ error }) =>
   ({ type: types.APP_SETTINGS_FAILED, error });
+
+export const settingUpdated = ({ _id, namespace, fields }) =>
+  ({ type: types.SETTING_UPDATED, _id, namespace, fields });
