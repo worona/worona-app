345fd1317f92a17869486669e0e03b25a220231e
Luis Herranz <luisherranz@gmail.com>
2017-01-10 13:39:58 +0100

    Fix actions and types export in settings.

--- client/packages/core-app-worona/src/app/settings-app-extension-worona/index.js
+++ client/packages/core-app-worona/src/app/settings-app-extension-worona/index.js
@@ -1,8 +1,12 @@
+import * as actions from './actions';
+import * as types from './types';
 import * as reducers from './reducers';
 import * as sagas from './sagas';
 import * as selectorCreators from './selectorCreators';
 
 export {
+  actions,
+  types,
   reducers,
   sagas,
   selectorCreators,
