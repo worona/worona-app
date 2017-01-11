9dd38ad26dd1381060959422baa9b48c5e8896a7
Luis Herranz <luisherranz@gmail.com>
2017-01-11 17:52:34 +0100

    Use new loader.

--- client/packages/core-app-worona/src/app/loading-app-theme-worona/components/Home/index.js
+++ client/packages/core-app-worona/src/app/loading-app-theme-worona/components/Home/index.js
@@ -3,13 +3,10 @@ import style from './style.css';
 
 export const Home = () => (
   <div>
-      <div className={style.messages}>
-        <div className={style.spinner}>
-          <div className={style.bounce1}></div>
-          <div className={style.bounce2}></div>
-          <div className={style.bounce3}></div>
-        </div>
-    </div>
+    <div className={`${style.loader} ${style.loader1}`}></div>
+    <span className={style.textBlue}>
+      Loading app
+    </span>
   </div>
 );
 
