dffa5df96f69a4313a2e92d44f411ba28f836718
Luis Herranz <luisherranz@gmail.com>
2017-01-30 10:37:59 +0100

    Add deep url actions.

--- client/packages/core-app-worona/src/app/router-app-extension-worona/types/index.js
+++ client/packages/core-app-worona/src/app/router-app-extension-worona/types/index.js
@@ -1,3 +1,4 @@
 export const ROUTER_DID_CHANGE = '@@reduxReactRouter/routerDidChange';
 export const SITE_ID_CHANGED = 'router/SITE_ID_CHANGED';
 export const IS_PREVIEW = 'router/IS_PREVIEW';
+export const DEEP_URL_VISITED = 'router/DEEP_URL_VISITED';
