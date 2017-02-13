d058ba00a7d68832906f9b95d6feec829b177ee4
Luis Herranz <luisherranz@gmail.com>
2017-02-13 14:33:11 +0100

    Start work on virtualPageViews (WIP)

--- client/packages/core-app-worona/src/app/gtm-app-extension-worona/sagas/index.js
+++ client/packages/core-app-worona/src/app/gtm-app-extension-worona/sagas/index.js
@@ -10,6 +10,20 @@ export function launchGTMEventsSaga({ type, ...props }) {
   });
 }
 
+export function* virtualPageView() {
+  const pathname = yield select(deps.selectors.getPathname);
+  const titleFromUrl = capitalize(/\/?([^/]+)/.exec(pathname)[1]).replace(/-/g, ' ');
+  const title = !service ? titleFromUrl : titleFromPkg;
+  const url = pathname.replace(/(\/?.+)(\/[a-zA-Z0-9]{17})/, '$1');
+  window.dataLayer.push({
+    event: 'virtualPageView',
+    virtualPage: {
+      title,
+      url,
+    },
+  });
+}
+
 export default function* gtmSagas() {
   yield take(deps.types.SITE_ID_CHANGED);
   const isPreview = yield select(deps.selectors.getPreview);
