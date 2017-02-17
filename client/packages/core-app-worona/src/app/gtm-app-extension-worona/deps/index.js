63d8fbc3b659ad593f7131879d12a4463ace3d2e
Luis Herranz <luisherranz@gmail.com>
2017-02-17 19:27:55 +0100

    Google Analytics working.

--- client/packages/core-app-worona/src/app/gtm-app-extension-worona/deps/index.js
+++ client/packages/core-app-worona/src/app/gtm-app-extension-worona/deps/index.js
@@ -2,8 +2,16 @@ import { dep } from 'worona-deps';
 
 export const types = {
   get SITE_ID_CHANGED() { return dep('router', 'types', 'SITE_ID_CHANGED'); },
+  get ROUTER_DID_CHANGE() { return dep('router', 'types', 'ROUTER_DID_CHANGE'); },
+  get APP_SETTINGS_SUCCEED() { return dep('settings', 'types', 'APP_SETTINGS_SUCCEED'); },
 }
 
 export const selectors = {
   get getPreview() { return dep('router', 'selectors', 'getPreview'); },
+  get getPathname() { return dep('router', 'selectors', 'getPathname'); },
+  get getURLQueries() { return dep('router', 'selectors', 'getURLQueries'); },
+};
+
+export const selectorCreators = {
+  get getSetting() { return dep('settings', 'selectorCreators', 'getSetting'); },
 };
--- client/packages/core-app-worona/src/app/gtm-app-extension-worona/sagas/index.js
+++ client/packages/core-app-worona/src/app/gtm-app-extension-worona/sagas/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 import { takeEvery } from 'redux-saga';
-import { select, take } from 'redux-saga/effects'
+import { select, take, fork, call } from 'redux-saga/effects';
 import * as deps from '../deps';
 
-export function launchGTMEventsSaga({ type, ...props }) {
+export function launchGtmEventsSaga({ type, ...props }) {
   window.dataLayer.push({
     event: type,
     props,
@@ -12,9 +12,38 @@ export function launchGTMEventsSaga({ type, ...props }) {
 
 export function* virtualPageView() {
   const pathname = yield select(deps.selectors.getPathname);
-  const titleFromUrl = capitalize(/\/?([^/]+)/.exec(pathname)[1]).replace(/-/g, ' ');
-  const title = !service ? titleFromUrl : titleFromPkg;
-  const url = pathname.replace(/(\/?.+)(\/[a-zA-Z0-9]{17})/, '$1');
+  const query = yield select(deps.selectors.getURLQueries);
+  const siteUrl = yield select(deps.selectorCreators.getSetting('generalSite', 'url'));
+  const siteName = yield select(deps.selectorCreators.getSetting('generalSite', 'name'));
+  let url = `${siteUrl}${pathname}`;
+  let title = `${siteName} - `;
+  if (query.p) {
+    title += `Post - ${query.p}`;
+    url += `?p=${query.p}`;
+  } else if (query.cat) {
+    title += `Category - ${query.cat}`;
+    url += `?cat=${query.cat}`;
+  } else if (query.tag) {
+    title += `Tag - ${query.tag}`;
+    url += `?tag=${query.tag}`;
+  } else if (query.author) {
+    title += `Author - ${query.author}`;
+    url += `?author=${query.author}`;
+  } else if (query.y || query.m) {
+    title += `Archive - ${query.m}`;
+    url += `?m=${query.m}`;
+  } else if (query.page_id) {
+    title += `Page - ${query.page_id}`;
+    url += `?page_id=${query.page_id}`;
+  } else if (query.s) {
+    title += `Search - ${query.s}`;
+    url += `?s=${query.s}`;
+  } else if (query.attachment_id) {
+    title += `Attachment - ${query.attachment_id}`;
+    url += `?attachment_id=${query.attachment_id}`;
+  } else {
+    title += 'Home';
+  }
   window.dataLayer.push({
     event: 'virtualPageView',
     virtualPage: {
@@ -26,10 +55,15 @@ export function* virtualPageView() {
 
 export default function* gtmSagas() {
   yield take(deps.types.SITE_ID_CHANGED);
-  const isPreview = yield select(deps.selectors.getPreview);
+  // const isPreview = yield select(deps.selectors.getPreview);
 
-  if (!isPreview) {
-    window.dataLayer = window.dataLayer || [];
-    yield takeEvery('*', launchGTMEventsSaga);
-  }
+  window.dataLayer = window.dataLayer || [];
+  yield [
+    fork(function* firstVirtualPageView() {
+      yield take(deps.types.APP_SETTINGS_SUCCEED);
+      yield call(virtualPageView);
+    }),
+    takeEvery('*', launchGtmEventsSaga),
+    takeEvery(deps.types.ROUTER_DID_CHANGE, virtualPageView),
+  ];
 }
