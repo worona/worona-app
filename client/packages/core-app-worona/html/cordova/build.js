73762657be7722eb48a8cafb5a12f7ab8daf8404
Luis Herranz <luisherranz@gmail.com>
2017-06-02 17:40:28 +0200

    Let's try to use this.

--- client/packages/core-app-worona/html/cordova/build.js
+++ client/packages/core-app-worona/html/cordova/build.js
@@ -1,8 +1,22 @@
 import { spawn } from 'child-process-promise';
+import replace from 'replace-in-file';
+import { readFile, copy, remove } from 'fs-extra';
 
 const build = async () => {
-
-
+  console.log('\n=> Building webpack.\n');
+  await spawn('webpack', [ '-p' ], { stdio: 'inherit' });
+  console.log('\n=> Reading bundle.js.\n');
+  const bundle = await readFile('bundle.js');
+  console.log('=> Deleting old index.html.\n');
+  await remove('index.html');
+  console.log('=> Duplicating index-template.html.\n');
+  await copy('index-template.html', 'index.html');
+  console.log('=> Replacing index.html.\n');
+  await replace({
+    files: 'index.html',
+    from: /###JAVASCRIPT###/,
+    to: bundle,
+  })
 }
 
 build();
